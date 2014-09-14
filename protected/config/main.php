ad6ac945e24d3f28b4258b7cc433997170e00f20
Sergius411 <hrapchun-sergiy@mail.ru>
2014-09-14 13:38:53 +0300

    добавив клас User

--- protected/config/main.php
+++ protected/config/main.php
@@ -17,7 +17,7 @@ return array(
 		'application.models.*',
 		'application.components.*',
 	),
-
+    
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
 		/*
---
+++ protected/models/db/user.php
@@ -0,0 +1,9 @@
+<?php
+
+class User extends CActiveRecord
+{
+    public static function model($className=__CLASS__)
+    {
+        return parent::model($className);
+    }
+}
