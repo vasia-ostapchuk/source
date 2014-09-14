0c3c0b0249de5aef101ae5f9f4058dfb5e8ca29d
taras.vilgan <Ostapchyk.Vasia@gmail.com>
2014-09-14 15:29:26 +0300

    тарас
    пробуэм

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -4,7 +4,7 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
-        
+        <link href="../../../css/style.css" rel="stylesheet" type="text/css"></link>
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
@@ -12,8 +12,8 @@
 
     <div class="container" id="page">
         <div id="header">
-                <div id="logo">
-                </div>
+            <div class="logo">
+            </div>
         </div>
 
         <div id="content">            
