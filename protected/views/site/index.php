1de82699988f8171f5367152d7366f3147964d01
vasia-ostapchuk <Ostapchyk.Vasia@gmail.com>
2014-09-14 20:59:44 +0300

    працює тестова модель таблиці user (Василь)

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,4 +1,20 @@
 
 Empty Start page's
 
-<?php $user = new User; echo "<pre>"; var_dump($user); echo "</pre>"; ?>
+<?php 
+    $user = new User; 
+    echo "<pre>"; 
+    var_dump($user); 
+    echo "</pre>"; 
+    $user->sex_id=3;
+    $user->state_id=3;
+    
+    $user->save();
+    
+    echo "<br>";
+    
+    echo "<pre>"; 
+    var_dump($user); 
+    echo "</pre>"; 
+    
+    ?>
