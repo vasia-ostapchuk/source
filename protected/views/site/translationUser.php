734df9dbfec80b194730d13d5ec65ed6ec8c4f53
taras.vilgan <taras.vilgan@gmail.com>
2014-09-30 16:43:56 +0300

    початок

--- protected/views/site/translationUser.php
+++ protected/views/site/translationUser.php
@@ -6,11 +6,34 @@
 
  <div class="translation_user">
      <aside>
-         <ul>
-             
-         </ul>
+         <form>
+             <button>Сутності</button>
+             <button>Локейшени</button>
+             <button>Типи</button>
+         </form>
      </aside>
      <article>
+         <div>
+             <button>EN</button>
+             <button>UA</button>
+             <button>PL</button>
+             <button>Пошку</button>
+             
+         </div>
+         <div class="sorting">
+             
+         </div>
+         <div class="">
+             <div class="">
+                 
+             </div>
+             <div class="">
+                 
+             </div>
+             <div class="">
+                 
+             </div>
+         </div>
          
      </article>
  </div>
