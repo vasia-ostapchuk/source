cc30568cbc7f2d7e901d25dc4a3b5ea9d02a1d86
Stepan <Stepankrupa23@gmail.com>
2014-10-07 23:20:31 +0300

    Stepan
    artist

--- protected/views/site/artist.php
+++ protected/views/site/artist.php
@@ -1,1 +1,19 @@
-<?php> echo "kkkkkkkk";
+<div class="singer_page">
+		<div class="singer_poster"> gfgf  </div>
+		<div class="singer_name">  назва  </div>
+		<div class="singer_style">   стиль </div>
+		<div class="singer_description">  опис  </div>
+		<div class="singer_site">  сайт  </div>
+		<div class="singer_right_block">   
+			<div class="singer_video">  відео  </div>
+			<div class="q1">  1 </div>
+			<div class="q2">  2  </div>
+			<div class="singer_album">  альбом  </div>
+			<div class="q11"> 11   </div>
+			<div class="q22">   22 </div>
+			<div class="singer_repertyar"> репертуар   </div>
+			<div class="singer_song1"> пісня1   </div>
+			<div class="singer_song2"> пісня2   </div>
+			<div class="singer_song3"> пісня3   </div>
+			</div>
+	</div>
