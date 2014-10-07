593bcd8981d0408663fd4d64aaf826f6804b6a4e
Stepan <Stepankrupa23@gmail.com>
2014-10-08 00:59:29 +0300

    stepan
    доповнення

--- protected/views/site/artist.php
+++ protected/views/site/artist.php
@@ -5,15 +5,15 @@
 		<div class="singer_description">  опис  </div>
 		<div class="singer_site">  сайт  </div>
 		<div class="singer_right_block">   
-			<div class="singer_video">  відео  </div>
+                    <div class="singer_video">  <a href=" " >video</a> </div>
 			<div class="q1">  1 </div>
 			<div class="q2">  2  </div>
-			<div class="singer_album">  альбом  </div>
-			<div class="q11"> 11   </div>
-			<div class="q22">   22 </div>
-			<div class="singer_repertyar"> репертуар   </div>
-			<div class="singer_song1"> пісня1   </div>
-			<div class="singer_song2"> пісня2   </div>
-			<div class="singer_song3"> пісня3   </div>
+                        <div class="singer_album">  <a href="">альбом</a>  </div>
+                        <div class="q11"> <img src="../../../images/g11.jpg" style="max-width: 250px;"/></div>
+                        <div class="q22"> <img src="../../../images/g22.jpg" style="max-width: 200px;"/> </div>
+                        <div class="singer_repertyar"> <a href="">репертуар</a>   </div>
+                        <div class="singer_song1">  <img src="../../../images/music.png" style="max-width: 12px;"/> Вже не сам  </div>
+			<div class="singer_song2"> <img src="../../../images/music.png" style="max-width: 12px;"/> Вже не сам    </div>
+			<div class="singer_song3"> <img src="../../../images/music.png" style="max-width: 12px;"/> Вже не сам  </div>
 			</div>
 	</div>
