2c86319fb2d2c945cb6a7f86448089953fe701df
taras.vilgan <taras.vilgan@gmail.com>
2014-10-09 02:01:17 +0300

    .....

--- protected/views/site/event.php
+++ protected/views/site/event.php
@@ -1,10 +1,21 @@
-
-    <div class="event-link">
-        <ul>
-            <li><a href="">Підписані</a></li>
-            <li><a href="">Друзі</a></li>
-        </ul>
+<div class="event-link">
+    <li><?php echo CHtml::link('Підписані', array('controller/action')); ?></li>
+    <li><?php echo CHtml::link('Друзі', array('controller/action')); ?></li>
+</div>
+<div class="eventsUsers">
+    <div class="checkbox">
+        <?php echo CHtml::CheckBox('checkbox',false, array (
+            'value'=>'on',
+        )); ?>
+        <span>Купили квиток</span>
     </div>
-    <div class="radio">
-
+    <div class="user-data">
+        <div class="avatar">
+            <?php echo CHtml::image('../../../images/testAvatar.jpg','назва',
+                array(
+                'class'=>'avatar',
+            )); ?>           
+        </div>
+        <span class='user-name'></span>
     </div>
+</div>
