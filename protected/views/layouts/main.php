90bdff58fb212c9d70e331d21c8639eddcdf192c
vasia-ostapchuk <Ostapchyk.Vasia@gmail.com>
2014-10-08 19:26:56 +0300

    ...

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -19,11 +19,12 @@
             </div>
             <div class="lan">
               <ul id="nav">  
- 
                   <li><a href=""title="ua"><img src="../../../images/ua.jpg" /></a>  
-<ul>  
-    <li class="uz"><a href=""title="pl"><img src="../../../images/pl.jpg" /></a> </li>  
-    <li class="uz"><a href=""title="en"><img src="../../../images/en.jpg" /></a> </li>  
+                    <ul>  
+                        <li class="uz"><a href=""title="pl"><img src="../../../images/pl.jpg" /></a> </li>  
+                        <li class="uz"><a href=""title="en"><img src="../../../images/en.jpg" /></a> </li>  
+                    </ul>
+              </ul>
             </div>
              
             <div class="user_reg_buttons">
