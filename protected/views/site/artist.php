a879710fc33f0c7ce59899a535fa627a9208db15
Stepan <Stepankrupa23@gmail.com>
2014-10-08 23:12:16 +0300

    stepan
    poster-singer (w)

--- protected/views/site/artist.php
+++ protected/views/site/artist.php
@@ -1,13 +1,20 @@
 <div class="singer_page">
-		<div class="singer_poster"> aa </div>
-		<div class="singer_name">  назва  </div>
+    <div class="singer_left_block">
+		<div class="singer_poster">
+                <?php echo CHtml::image('../../../images/2poster.jpg','назва',
+array(
+'class'=>'poster_singer',
+)); ?>           
+                </div>
+    <div class="singer_name">  назва  </div>
 		<div class="singer_style">   стиль </div>
 		<div class="singer_description">  опис  </div>
 		<div class="singer_site">  сайт  </div>
+    </div>
 		<div class="singer_right_block">   
                     <div class="singer_video">  <a href=" " >video</a> </div>
-			<div class="q1">  1 </div>
-			<div class="q2">  2  </div>
+			<div class="q1">  <iframe width="245" height="160" src="//www.youtube.com/embed/1-HHNZYIGI0" frameborder="0" allowfullscreen></iframe> </div>
+			<div class="q2">  <iframe width="245" height="160" src="//www.youtube.com/embed/1-HHNZYIGI0" frameborder="0" allowfullscreen></iframe>  </div>
                         <div class="singer_album">  <a href="">альбом</a>  </div>
                         <div class="q11"> <img src="../../../images/g11.jpg" style="max-width: 250px;"/></div>
                         <div class="q22"> <img src="../../../images/g22.jpg" style="max-width: 200px;"/> </div>
