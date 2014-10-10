774f26896ac1f699aedeb6d7c0052f692cbc5582
taras.vilgan <taras.vilgan@gmail.com>
2014-10-10 04:17:36 +0300

    не повністю заповнена сторінка event

--- protected/views/site/event.php
+++ protected/views/site/event.php
@@ -1,4 +1,4 @@
-<div class="filters">
+<aside>
     <?php
         $form = $this->beginWidget('CActiveForm', array(
             'id' => 'FilterForm',
@@ -14,7 +14,7 @@
         $model= new FilterForm;
     ?>
         <div class="filters-label">
-            Фільтри:
+            Пошук виконавця(В розробці)
         </div>
         <div class="filter-label">
             <?php echo $form->labelEx($model,'country') . "\n"; ?>
@@ -40,7 +40,79 @@
             ?>
             <?php echo $form->error($model,'style') . "\n"; ?>
         </div>
-        <div id="bottom-block" style="background: #777; color: #FFF;"></div>
+        <div class="checkbox">
+            <?php echo CHtml::CheckBox('checkbox',false, array (
+                'value'=>'on',
+            )); ?>
+            <span>Підстиль1</span>
+        </div>
+        <div class="checkbox">
+            <?php echo CHtml::CheckBox('checkbox',false, array (
+                'value'=>'on',
+            )); ?>
+            <span>Підстиль2</span>
+        </div>
+        <div class="checkbox">
+            <?php echo CHtml::CheckBox('checkbox',false, array (
+                'value'=>'on',
+            )); ?>
+            <span>Підстиль3</span>
+        </div>
+        <div class="checkbox">
+            <?php echo CHtml::CheckBox('checkbox',false, array (
+                'value'=>'on',
+            )); ?>
+            <span>Підстиль4</span>
+        </div>
+        <div class="checkbox">
+            <?php echo CHtml::CheckBox('checkbox',false, array (
+                'value'=>'on',
+            )); ?>
+            <span>Підстиль5</span>
+        </div>
             <?php
             $this->endWidget(); ?>
-</div>
+</aside>
+<article>
+    <div class="current-events">
+        <div class="event">
+
+        </div>
+        <div class="event">
+
+        </div>
+        <div class="event">
+
+        </div>
+    </div>
+    <div class="status-event">
+        <span>Стан події відносно системи</span>
+    </div>
+    <div class="singer_left_block">
+        <div class="singer_poster">
+            <?php echo CHtml::image('../../../images/2poster.jpg','назва',
+                array(
+                'class'=>'poster_singer',
+            )); ?>           
+        </div>
+        <div class="singer_name">  
+            <?php echo CHtml::textField('Text', 'Друга ріка',
+                array('disabled'=>'disabled','class'=>'name_poster')); ?>
+        </div>
+        <div class="page-link">
+            <a href="">Перехід на сторінку</a>
+        </div>
+        <div class="singer_style"> 
+            <p><span class="style_singer">Стилі</span></p>
+            <ul>
+                <li>Джаз</li>
+                <li>Блюз</li>
+                <li>Реп</li>
+                <li>Поп</li>
+            </ul>
+        </div>
+    </div>
+    <div class="buttons">
+        
+    </div>
+</article>
