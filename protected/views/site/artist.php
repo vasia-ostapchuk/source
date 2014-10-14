3a2f82746a1cc528aa380fb4b33a08abe0d04068
vasia-ostapchuk <Ostapchyk.Vasia@gmail.com>
2014-10-14 22:15:00 +0300

    спроба 2 (Василь)

--- protected/views/site/artist.php
+++ protected/views/site/artist.php
@@ -1,4 +1,46 @@
 
+<script type="text/javascript">
+    // При наведении на ссылку
+    $(document).ready(function(){
+    $(".link_test").mouseover
+    (function(){
+    // Получаем ID блока, который нужно показать
+    //var title = $(this).attr("title");
+
+    // Показываем блок
+    //$(this).append( $(title) );
+    $("#title_for_first_link").fadeIn();
+    });
+
+    // При уходе мышки со ссылки
+    $(".link_test").mouseout
+    (function(){
+
+    // Получаем ID блока, который нужно показать
+    //var title = $(this).attr("title");
+
+    // Скрываем блок
+    $("#title_for_first_link").fadeOut();
+
+    });
+    });
+</script>
+<style>
+        .link_test
+        {
+          //position:relative;
+            }
+        
+        .title
+        {
+          display:none; height:100px; width:200px; background:#fffed2; border:1px solid #838231; padding:5px; position:absolute;
+          margin-left:120px; top:5px; z-index:100;
+            }
+        .index
+        {
+          position:absolute; height:20px; width:30px; background:red; left:-5px;
+            }
+    </style>
 <div class="singer_page">
   
     <div class="singer_left_block">
@@ -6,13 +48,16 @@
             <div class="singer_poster">
                
                 <?php echo CHtml::image('../../../images/2poster.jpg','назва',
-array(
-'class'=>'poster_singer','onclick'=>"", 'title'=>"Постер Друга ріка" 
+                    array(
+                    'class'=>'link_test poster_singer','onclick'=>"", 'title'=>"Постер Друга ріка" 
                         )); ?>         
                 
                 </div>
     <div class="singer_name">  
-
+    <div id="title_for_first_link" class="title">
+            <div class="index"></div>
+            Тут какой-нибудь HTML код для первой ссылки
+    </div>
    <?php echo CHtml::TextField('Text', 'Друга ріка',
     array('class'=>'name_poster','class'=>'singer_name')); ?>
 
