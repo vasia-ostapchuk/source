c9fd710a1f39cfc0c486799803e068f85ceda377
vasia-ostapchuk <Ostapchyk.Vasia@gmail.com>
2014-10-14 21:58:41 +0300

    налаштована подія mouserover для постера (Василь)

--- protected/views/site/artist.php
+++ protected/views/site/artist.php
@@ -1,4 +1,47 @@
+<script>
+    $(document).ready(function(){
 
+    // При наведении на ссылку
+    $(".link_test").mouseover
+    (function(){
+        //alert(1);
+    // Получаем ID блока, который нужно показать
+    //var title = $(this).attr("title");
+
+    // Показываем блок
+    //$(this).append( $(title) );
+    $("#title_for_first_link").fadeIn();
+    });
+
+    // При уходе мышки со ссылки
+    $(".link_test").mouseout
+    (function(){
+
+    // Получаем ID блока, который нужно показать
+    //var title = $(this).attr("title");
+
+    // Скрываем блок
+    $("#title_for_first_link").fadeOut();
+
+    });
+    });
+</script>
+<style>
+        .link_test
+        {
+          //position:relative;
+            }
+        
+        .title
+        {
+          display:none; height:100px; width:200px; background:#fffed2; border:1px solid #838231; padding:5px; position:absolute;
+          margin-left:120px; top:5px; z-index:100;
+            }
+        .index
+        {
+          position:absolute; height:5px; width:10px; background:red; left:-5px;
+            }
+    </style>
 <div class="singer_page">
   
     <div class="singer_left_block">
@@ -6,13 +49,16 @@
             <div class="singer_poster">
                
                 <?php echo CHtml::image('../../../images/2poster.jpg','назва',
-array(
-'class'=>'poster_singer','onclick'=>"", 'title'=>"Постер Друга ріка" 
-                        )); ?>         
-                
+                            array(
+                            'class'=>'link_test poster_singer','onclick'=>"", 'title'=>"Постер Друга ріка" 
+                        )); ?> 
                 </div>
+        
     <div class="singer_name">  
-
+        <div id="title_for_first_link" class="title">
+            <div class="index"></div>
+            Тут какой-нибудь HTML код для первой ссылки
+        </div>
    <?php echo CHtml::TextField('Text', 'Друга ріка',
     array('class'=>'name_poster','class'=>'singer_name')); ?>
 
