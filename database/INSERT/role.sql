1a3348dfbf2cd1258087eed80a95614256c3dad6
Sergius411 <hrapchun-sergiy@mail.ru>
2014-10-18 00:28:48 +0300

    виправлені інсерти

--- database/INSERT/role.sql
+++ database/INSERT/role.sql
@@ -1,6 +1,8 @@
-﻿
+
 INSERT INTO `role` (`id`, `name`, `alias`, `permission_list`) VALUES
 	(1, 'Admin', 'Admin', ',1'),
 	(2, 'User', 'Normal user', '2,3,4,5,6,7,8,9,10,11,12,13,14,15'),
-	(3, 'Super amdin', 'User with full access', '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15'),
-	(5, 'user2', 'for test', ',7,6,5,12');
+	(4, 'Guest', 'guest', ',3,2,4,5,6,7,8,9,10,11,13,14'),
+	(5, 'Moderator', 'Moderator for artist', NULL),
+	(8, 'Transliter', 'Transliter', NULL),
+	(10, 'Artist', 'artist or band', NULL);
