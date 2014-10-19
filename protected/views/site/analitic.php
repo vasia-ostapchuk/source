fd2b1a1ca30a9562747eaf1587694469c9e3d5f1
Stepan <Stepankrupa23@gmail.com>
2014-10-19 23:35:02 +0300

    analitic(test)

--- protected/views/site/analitic.php
+++ protected/views/site/analitic.php
@@ -25,36 +25,55 @@
          
     <div class="analitic-calendar">
 
-   <?php
-$this->widget('zii.widgets.jui.CJuiDatePicker',array(
-    'language'=>'uk',
-    'name'=>'datepicker-month-year-menu',
-    'value'=>Filter::getCalendarDate(),
-    'flat'=>true,//remove to hide the datepicker
-    'options'=>array(
-	    'dateFormat' => 'yy-mm-dd',
-        'showAnim'=>'slide',//'slide','fold','slideDown','fadeIn','blind','bounce','clip','drop'
-        'changeMonth'=>true,
-        'changeYear'=>true,
-        'yearRange'=>'2000:2099',
-        'minDate' => '2000-01-01',      // minimum date
-        'maxDate' => '2099-12-31',      // maximum date
-        'onSelect'=>"js:function(dateText){"
-                            . "var date={date:dateText, id:'calendar'};"
-                            . "$.ajax({"
-                                    . " type: 'POST',"
-                                    . " data:  date,"
-                                    . " url:  'index.php?r=filter/calendar',"
-                                    . " success: function(){" 
-                                    . "}"
-                                . "});"
-                        . "}",
-    ),
-    'htmlOptions'=>array(
-        'class'=>'widget-calendar',
-        ),
-)); ?>
-        
+ <table id="analitic-calendar">
+  <thead>
+    <tr><td>‹<td colspan="5"><td>›
+    <tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс
+  <tbody>
+</table>
+
+<script>
+function Calendar2(id, year, month) {
+var Dlast = new Date(year,month+1,0).getDate(),
+    D = new Date(year,month,Dlast),
+    DNlast = new Date(D.getFullYear(),D.getMonth(),Dlast).getDay(),
+    DNfirst = new Date(D.getFullYear(),D.getMonth(),1).getDay(),
+    calendar = '<tr>',
+    month=["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"];
+if (DNfirst != 0) {
+  for(var  i = 1; i < DNfirst; i++) calendar += '<td>';
+}else{
+  for(var  i = 0; i < 6; i++) calendar += '<td>';
+}
+for(var  i = 1; i <= Dlast; i++) {
+  if (i == new Date().getDate() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
+    calendar += '<td class="today">' + i;
+  }else{
+    calendar += '<td>' + i;
+  }
+  if (new Date(D.getFullYear(),D.getMonth(),i).getDay() == 0) {
+    calendar += '<tr>';
+  }
+}
+for(var  i = DNlast; i < 7; i++) calendar += '<td>&nbsp;';
+document.querySelector('#'+id+' tbody').innerHTML = calendar;
+document.querySelector('#'+id+' thead td:nth-child(2)').innerHTML = month[D.getMonth()] +' '+ D.getFullYear();
+document.querySelector('#'+id+' thead td:nth-child(2)').dataset.month = D.getMonth();
+document.querySelector('#'+id+' thead td:nth-child(2)').dataset.year = D.getFullYear();
+if (document.querySelectorAll('#'+id+' tbody tr').length < 6) {  // чтобы при перелистывании месяцев не "подпрыгивала" вся страница, добавляется ряд пустых клеток. Итог: всегда 6 строк для цифр
+    document.querySelector('#'+id+' tbody').innerHTML += '<tr><td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;';
+}
+}
+Calendar2("analitic-calendar", new Date().getFullYear(), new Date().getMonth());
+// переключатель минус месяц
+document.querySelector('#analitic-calendar thead tr:nth-child(1) td:nth-child(1)').onclick = function() {
+  Calendar2("analitic-calendar", document.querySelector('#calendar2 thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#analitic-calendar thead td:nth-child(2)').dataset.month)-1);
+}
+// переключатель плюс месяц
+document.querySelector('#calendar2 thead tr:nth-child(1) td:nth-child(3)').onclick = function() {
+  Calendar2("analitic-calendar", document.querySelector('#analitic-calendar thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#analitic-calendar thead td:nth-child(2)').dataset.month)+1);
+}
+</script>   
     </div>
     
     <?php 
