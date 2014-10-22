1e1eab2fa9c042891c4d01c3b5375fb09afecec2
Stepan <Stepankrupa23@gmail.com>
2014-10-22 21:18:13 +0300

    analitic

--- protected/views/site/analitic.php
+++ protected/views/site/analitic.php
@@ -1,62 +1,62 @@
-<div class="analitic-content-site">
-    <div class="analitic-left-block">
-        <div class="analitic_filters"> 
-        
-         <?php
-            echo CHtml::dropDownList('analitic-country', '', array('Країна'),array());
 
-            ?>
-          
+    <div class="analitic-left-block">
+        <div class="analitic_filters">         
+            <?php
+            echo CHtml::dropDownList('analitic-country', '', array('Країна'),array());
+            ?>          
             <?php
             echo CHtml::dropDownList('analitic-citi', '', array('Місто'),array());
-            ?>
-          
+            ?>          
             <?php
-
             echo CHtml::dropDownList('analitic-style', '', array('Стиль'),array());
             ?>
-            
             <?php
             echo CHtml::dropDownList('analitic-substyle', '', array('Підстиль'),array());
             ?>
-        
-        
         </div>
-            <div class="analitic-calendar">
-                    <?php
-$this->widget('zii.widgets.jui.CJuiDatePicker',array(
-    'name'=>'datepicker-month-year-menu',
-    'flat'=>true,//remove to hide the datepicker
-    'options'=>array(
-	    'dateFormat' => 'yy-mm-dd',
-        'showAnim'=>'slide',//'slide','fold','slideDown','fadeIn','blind','bounce','clip','drop'
-        'changeMonth'=>true,
-        'changeYear'=>true,
-        'yearRange'=>'2000:2099',
-        'minDate' => '2000-01-01',      // minimum date
-        'maxDate' => '2099-12-31',      // maximum date
-    ),
-    'htmlOptions'=>array(
-        'style'=>''
-    ),
-));
-?>
+            <div class="calendar">
+                <?php
+                        $this->widget('zii.widgets.jui.CJuiDatePicker',array(
+                            'language'=>'uk',
+                            'name'=>'datepicker-month-year-menu',
+                            'value'=>Filter::getCalendarDate(),
+                            'flat'=>true,//remove to hide the datepicker
+                            'options'=>array(
+                                    'dateFormat' => 'yy-mm-dd',
+                                'showAnim'=>'slide',//'slide','fold','slideDown','fadeIn','blind','bounce','clip','drop'
+                                'changeMonth'=>true,
+                                'changeYear'=>true,
+                                'yearRange'=>'2000:2099',
+                                'minDate' => '2000-01-01',      // minimum date
+                                'maxDate' => '2099-12-31',      // maximum date
+                                'onSelect'=>"js:function(dateText){"
+                                                    . "var date={date:dateText, id:'calendar'};"
+                                                    . "$.ajax({"
+                                                            . " type: 'POST',"
+                                                            . " data:  date,"
+                                                            . " url:  'index.php?r=filter/calendar',"
+                                                            . " success: function(){" 
+                                                            . "}"
+                                                        . "});"
+                                                . "}",
+                            ),
+                            'htmlOptions'=>array(
+                                'class'=>'widget-calendar',
+                                ),));
+                ?>
             </div>
                 <div class="analitic-reports">
                     <?php echo CHtml::link('Звіти по параметрах',array('controller/action')); ?>
                 </div>
     </div>
-</div>
-    
         <div class="analitic-top">
             <div class="analitic-navigator">
-        <div class="analitic-navigator-search">
-        
-        <form class="form-search-analitic" method="post" action="index.php?r=site/search">
-            <input type="search" name="search" placeholder="пошук" value=""/>
-            <input id="link" type="hidden" name ="id" value="google" />
-            <input type="submit" value=""/>
-        </form>
+    <div class="analitic-navigator-search">
+            <form class="form-search-analitic" method="post" action="index.php?r=site/search">
+                <input type="search" name="search" placeholder="пошук" value=""/>
+                <input id="link" type="hidden" name ="id" value="google" />
+                <input type="submit" value=""/>
+            </form>
         <script>  
             $(document).ready(function(){ //відбувається після завантаження сторінки
 
@@ -88,15 +88,14 @@ $this->widget('zii.widgets.jui.CJuiDatePicker',array(
     </div>
         </div>
             <div class="analitic-sorting">Сортувати:
-            <label id="analitic_sorting_label" title="Сортувати"> 
-            <?php echo CHtml::dropDownList('analitic_sorting_label', 'analitic_sorting_label', 
-                array('сортувати' => 'Сортувати',  'Сортувати' => 'Сщртувати')
-                    );
-             ?>
-            </label>
+                <label id="analitic_sorting_label" title="Сортувати"> 
+                <?php echo CHtml::dropDownList('analitic_sorting_label', 'analitic_sorting_label', 
+                    array('сортувати' => 'Сортувати',  'Сортувати' => 'Сщртувати')
+                        );
+                 ?>
+                </label>
             </div>
         </div>
-
             <div class="analitic-content">
                 <div class="analitic-content-description">
                     <div class="analitic-singer-name">Виконавець </div>
@@ -161,4 +160,3 @@ $this->widget('zii.widgets.jui.CJuiDatePicker',array(
                 </div>
             </div>
     
-</div>
